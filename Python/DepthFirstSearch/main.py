e3f42cc497bb5af54e7c4be2cc5091b152d61d1d
Derk-B <derkblom02@gmail.com>
2023-04-22 08:33:00 +0200

    Added DFS in Python

---
+++ Python/DepthFirstSearch/main.py
@@ -0,0 +1,27 @@
+from Tree import Node
+
+
+def search(value, node: Node):
+    if (node == None):
+        return False
+
+    if (node.value == value):
+        return True
+
+    return search(value, node.left) or search(value, node.right)
+
+
+# Sample tree
+tree = Node(1,
+            Node(2,
+                 Node(5, None, None),
+                 None),
+            Node(7,
+                 Node(3, None, None),
+                 None)
+            )
+
+# Sample input
+print(search(7, tree))
+print(search(5, tree))
+print(search(8, tree))
